0e0c213c2400699fd2a7a56b3a4fdadecfaa8e2e
Florian Kapaun <hello@florian-kapaun.de>
2021-02-05 15:56:07 +0100

    :mag: noindex for error and imprint pages

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php e($page->isUnlisted() && !$page->is('home'), '<meta name="robots" content="noindex">'); ?>
     <meta name="description" content="<?= $site->description() ?>">
     <meta name="keywords" content="<?= $site->keywords() ?>">
     <title><?= $page->title()->html() ?> | <?= $site->title()->html() ?></title>
