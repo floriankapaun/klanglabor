0cf5ac235f1b876593aa72236852a1252faed814
Florian Kapaun <hello@florian-kapaun.de>
2021-02-05 16:34:37 +0100

    :lipstick: random bg-color on spacings

--- site/snippets/layouts.php
+++
@@ -1,9 +0,0 @@
-<?php foreach ($field->toLayouts() as $layout): ?>
-    <article id="<?= $layout->id() ?>" class="row">
-        <?php foreach ($layout->columns() as $column): ?>
-            <section class="column column-<?= str_replace('/', '-', $column->width()) ?>" style="--columns: <?= $column->span() ?>">
-                <?php e($column->blocks()->isEmpty(), '<div class="spacing"></div>', $column->blocks()) ?>
-            </section>
-        <?php endforeach ?>
-    </article>
-<?php endforeach ?>
--- site/templates/default.php
+++ site/templates/default.php
@@ -3,11 +3,11 @@
 <main>
     <canvas id="generative_pattern"></canvas>
     <article class="row">
-        <section class="column column-1-1">
+        <section class="column column-1-1 bg-paper">
             <h1><?= $page->title()->html() ?></h1>
         </section>
     </article>
-    <?php snippet('layouts', ['field' => $page->layout()])  ?>
+    <?php snippet('body', ['field' => $page->layout()])  ?>
 </main>
 
 <?php snippet('footer') ?>
