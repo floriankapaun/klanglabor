a9f3c76ee9b3637e6691fae0b0bbc93156a2f507
Florian Kapaun <hello@florian-kapaun.de>
2021-02-05 15:15:12 +0100

    :bento: added imprint and data privacy info

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,10 @@
-    <footer>
+    <footer class="caps">
+        <section class="block block-xl">
+            <a href="/impressum">Impressum</a>
+        </section>
+        <section class="block block-xl">
+            <a href="https://www.hs-augsburg.de/Service/Datenschutz.html" target="_blank" rel="noreferrer">Datenschutz</a>
+        </section>
         <!-- Config variables -->
         <script type="text/javascript">
             const CONFIG = {
