534da38d01058c88de589edcaa5ad90253d30ec8
Florian Kapaun <hello@florian-kapaun.de>
2021-02-06 10:54:55 +0100

    :children_crossing: implemented navigation auto-closing logic

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,5 @@
 const NAV = document.getElementById('mainNav');
 const NAV_TOGGLE = document.getElementById('mainNavToggle');
-const NAV_SECTIONS = document.getElementsByClassName('nav-section');
 const NAV_LINKS = document.getElementsByClassName('nav-link');
 
 
@@ -16,11 +15,30 @@ const handleNavInteraction = (e, parent = e.target.parentNode, query = '.nav-sec
     if (subNav) {
         // If there is a sub-navigation prevent the routing and toggle it
         e.preventDefault();
-        const isExpanded = e.target.getAttribute('aria-expanded') === 'true';
-        e.target.setAttribute('aria-expanded', !isExpanded);
-        if (parent === NAV) parent = NAV_TOGGLE;
-        parent.classList.toggle('active');
-        subNav.classList.toggle('expanded');
+        const triggerElement = parent === NAV ? NAV_TOGGLE : e.target;
+        const isExpanded = triggerElement.getAttribute('aria-expanded') === 'true';
+
+        // Close all subnavs (only if trigger is not global NAV_TOGGLE)
+        if (triggerElement !== NAV_TOGGLE) {
+            for (const trigger of parent.parentNode.querySelectorAll('.nav-link')) {
+                trigger.setAttribute('aria-expanded', false);
+                trigger.classList.remove('active');
+            }
+            for (const nav of parent.parentNode.querySelectorAll(query)) {
+                nav.classList.remove('expanded');
+            }
+        }
+        // Toggle the trigger elements subnav
+        if (isExpanded) {
+            triggerElement.setAttribute('aria-expanded', false);
+            triggerElement.classList.remove('active');
+            subNav.classList.remove('expanded');
+        } else {
+            triggerElement.setAttribute('aria-expanded', true);
+            triggerElement.classList.add('active');
+            subNav.classList.add('expanded');
+        }
+
     }
 };
 
@@ -30,6 +48,18 @@ for (const navLink of NAV_LINKS) {
     navLink.addEventListener('click', handleNavInteraction);
 }
 
+// Add click listener for main NAV_TOGGLE for routing or toggling the sub navigation
 NAV_TOGGLE.addEventListener('click', (e) => {
     handleNavInteraction(e, NAV);
+});
+
+// A press on the ESC Key should close the main navigation
+document.addEventListener('keydown', (e) => {
+    const isEscape = 'key' in e ? (e.key === 'Escape' || e.key === 'Esc') : e.keyCode === 27;
+    if (isEscape) {
+        NAV_TOGGLE.setAttribute('aria-expanded', false);
+        NAV_TOGGLE.classList.remove('active');
+        NAV.querySelector('.nav-section').classList.remove('expanded');
+        NAV_TOGGLE.focus();
+    }
 });
