310da37a6c49be4d3b1ee18a1299a7496e2d8bf6
Florian Kapaun <hello@florian-kapaun.de>
2022-02-12 09:18:57 +0100

    :recycle: Menü -> Menue

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,13 +1,8 @@
 <button aria-expanded="false" aria-label="Main Navigation Toggle" id="mainNavToggle" class="block block-xl caps">
-    Menü
+    Menue
     <span class="sr-only">öffnen oder schließen</span>
 </button>
 
-<nav
-    id="mainNav"
-    class="caps"
-    aria-label="Main Navigation List"
-    role="navigation"
-    style="--depth: <?= $navDepth ?>">
+<nav id="mainNav" class="caps" aria-label="Main Navigation List" role="navigation" style="--depth: <?= $navDepth ?>">
     <?= $navHTML ?>
 </nav>
